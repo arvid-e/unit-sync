15557ec51e286bd5cf34ed1e995d649bd99ccfcd
arvid-e <arvid.edvinsson@gmail.com>
2025-11-26 11:02:53 +0900

    RED: Mock return values for services

--- test/UnitConverterImpl.spec.ts
+++ test/UnitConverterImpl.spec.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, it, vi } from 'vitest';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 import type { UnitConversionService } from '../src/interfaces/UnitConversionService';
 import type { UnitValidationService } from '../src/interfaces/UnitValidationService';
 import { UnitConverterImpl } from '../src/services/UnitConverterImpl';
@@ -7,25 +7,39 @@ type MockedConversionService = UnitConversionService & { calculate: ReturnType<t
 type MockedValidationService = UnitValidationService & {
   unitsAreConfigured: ReturnType<typeof vi.fn>;
   isValidValue: ReturnType<typeof vi.fn>;
+  isValidUnit: ReturnType<typeof vi.fn>;
   isConversionPossible: ReturnType<typeof vi.fn>;
   unitsHaveSameDimension: ReturnType<typeof vi.fn>;
 };
 
-describe('UnitConverterImpl', () => {
+describe('UnitConverter', () => {
   let mockConversionService: MockedConversionService;
-  let mockedValidationService: MockedValidationService;
+  let mockValidationService: MockedValidationService;
   let unitConverter: UnitConverterImpl;
+
+  const fromUnit = 'yard';
+  const toUnit = 'kilometer';
+  const value = 200;
+  const expectedResult = 0.18288;
 
   beforeEach(() => {
     mockConversionService = {
       calculate: vi.fn(),
     } as unknown as MockedConversionService;
 
-    mockedValidationService = {
+    mockValidationService = {
       getConversionConfig: vi.fn(),
     } as unknown as MockedValidationService;
 
-    unitConverter = new UnitConverterImpl(mockConversionService, mockedValidationService);
+    unitConverter = new UnitConverterImpl(mockConversionService, mockValidationService);
+
+    mockValidationService.unitsAreConfigured.mockReturnValue(true);
+    mockValidationService.isValidValue.mockReturnValue(true);
+    mockValidationService.isValidUnit.mockReturnValue(true);
+    mockValidationService.isConversionPossible.mockReturnValue(true);
+    mockValidationService.unitsHaveSameDimension.mockReturnValue(true);
+    mockConversionService.calculate.mockReturnValue(expectedResult);
   });
+
 
 });
