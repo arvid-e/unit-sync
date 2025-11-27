2b2b41c9da0fbbcc98676cf49bdc1da5ca928f08
arvid-e <arvid.edvinsson@gmail.com>
2025-11-27 12:47:38 +0900

    RED: Test if parseCommand returns input payload on correct command

--- test/ConsoleAppImpl.spec.ts
+++ test/ConsoleAppImpl.spec.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, vi } from 'vitest';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { UnitConverter } from '../src/interfaces/UnitConverter';
 import { ConsoleAppImpl } from '../src/services/ConsoleAppImpl';
 
@@ -14,5 +14,26 @@ describe('ConsoleAppImpl', () => {
     } as unknown as MockUnitConverter;
 
     consoleAppImpl = new ConsoleAppImpl(mockUnitConverter);
+  });
+
+  describe('parseCommand', () => {
+    const parseCommand = (input: string) => {
+      return (consoleAppImpl as any).parseCommand(input);
+    };
+
+    it('should return the input payload from a correct input string', () => {
+      const inputString = '500 yard to meter';
+
+      const expectedResult = {
+        value: 500,
+        fromUnit: 'yard',
+        toUnit: 'meter',
+      };
+
+      const result = parseCommand(inputString);
+
+      expect(result).toEqual(expectedResult);
+      expect(consoleAppImpl.parseCommand).toHaveBeenCalledTimes(1);
+    });
   });
 });
