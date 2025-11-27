e59f9a2e9f0029409b1a7c45bb2b56f2ac770119
arvid-e <arvid.edvinsson@gmail.com>
2025-11-27 11:48:29 +0900

    Instantiate the UnitConverterImpl in the index file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello World!');
+import { UnitConverterImpl } from "./services/UnitConverterImpl.js";
+import { UnitConversionServiceImpl } from "./services/UnitConversionServiceImpl.js";
+import { UnitValidationServiceImpl } from "./services/UnitValidationServiceImpl.js";
+import { InMemoryConversionConfigRepository } from "./repositories/InMemoryConversionConfigRepository.js";
+import { InMemoryUnitInfoRepository } from "./repositories/InMemoryUnitInfoRepository.js";
+
+const unitInfoRepo = new InMemoryUnitInfoRepository();
+const unitConversionConfigRepo = new InMemoryConversionConfigRepository();
+const unitConversionService = new UnitConversionServiceImpl(unitConversionConfigRepo);
+const unitValidationService = new UnitValidationServiceImpl(unitInfoRepo, unitConversionConfigRepo);
+
+const unitConverter = new UnitConverterImpl(unitConversionService, unitValidationService);
+
+
