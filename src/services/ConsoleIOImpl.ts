f12a0c75b017a8fed90bbd2efa03026ae7947113
arvid-e <arvid.edvinsson@gmail.com>
2025-12-05 15:00:55 +0900

    Create ConsoleIO implementation for reading and outputting to the terminal

---
+++ src/services/ConsoleIOImpl.ts
@@ -0,0 +1,42 @@
+import { stdin as input, stdout as output } from 'process';
+import * as readline from 'readline/promises';
+import type { ConsoleIO } from '../interfaces/ConsoleIO.js';
+
+export class ConsoleIOImpl implements ConsoleIO {
+  private rl: readline.Interface | null = null;
+
+  constructor() {}
+
+  public initialize(): void {
+    if (!this.rl) {
+      this.rl = readline.createInterface({ input, output });
+    }
+  }
+
+  public async readInput(input: string): Promise<string> {
+    if (!this.rl) {
+      this.initialize();
+    }
+
+    const answer = await this.rl!.question(input);
+
+    return answer;
+  }
+
+  public printOutput(output: string): void {
+    console.log(output);
+  }
+
+  public printError(error: string): void {
+    console.error(`ERROR: ${error}`);
+  }
+
+
+  public exit(): void {
+    if (this.rl) {
+      this.rl.close();
+    }
+
+    process.exit(0);
+  }
+}
